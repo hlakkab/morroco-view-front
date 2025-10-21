868c2c05b6fdf84d5b8fd857023a9099964e8641
mohcine sahtani <mohcine.sahtani@gmail.com>
2025-10-21 17:47:40 +0100

    hiding google oauth button for ios

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'Morocco View',
   slug: 'morocco-view',
-  version: '1.1.1',
+  version: '1.3.3',
   orientation: 'portrait',
   userInterfaceStyle: 'light',
   splash: {
