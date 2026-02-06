ff6145aa54742c81f6965fd45496f58a37facf99
mohcine sahtani <mohcine.sahtani@gmail.com>
2026-02-06 15:18:34 +0100

    updated the version to 1.5.0

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'Morocco View',
   slug: 'morocco-view',
-  version: '1.4.9',
+  version: '1.5.0',
   orientation: 'portrait',
   userInterfaceStyle: 'light',
   splash: {
